ca34fe8ee42852307ce82bfe9d7e7131e72ded78
ReAlMs710 <159969311+ReAlMs710@users.noreply.github.com>
2024-09-12 09:10:11 -0400

    calc gets updated

--- calc.cpp
+++ calc.cpp
@@ -3,13 +3,15 @@ using namespace std;
 
 int main() {
     char operation;
-    float num1, num2;
+    int num1, num2;
 
+    // Ask for user input
     cout << "Enter an operator (+, -, *, /, %): ";
     cin >> operation;
     cout << "Enter two numbers: ";
     cin >> num1 >> num2;
 
+    // Perform the operation
     switch(operation) {
         case '+':
             cout << num1 << " + " << num2 << " = " << num1 + num2 << endl;
@@ -27,10 +29,10 @@ int main() {
                 cout << "Error! Division by zero." << endl;
             break;
         case '%':
-            if(static_cast<int>(num2) != 0)
-                cout << static_cast<int>(num1) << " % " << static_cast<int>(num2) << " = " << static_cast<int>(num1) % static_cast<int>(num2) << endl;
+            if(num2 != 0)
+                cout << num1 << " % " << num2 << " = " << num1 % num2 << endl;
             else
-                cout << "Error! Division by zero." << endl;
+                cout << "Error! Modulus by zero." << endl;
             break;
         default:
             cout << "Error! Invalid operator." << endl;
